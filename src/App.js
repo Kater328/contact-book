8ee573316f403ed0ce5e5986bc78ce75804ab99a
kater328 <kattyzz328@gmail.com>
2021-09-05 23:03:21 +0300

    feat: implement router with header styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route, NavLink } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contacts from "./pages/Contacts";
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <div className="App">
+        <header>
+          <nav>
+            <NavLink to="/" exact>Home</NavLink>
+            <NavLink to="/about">About</NavLink>
+            <NavLink to="/contacts">Contacts</NavLink>
+          </nav>
+        </header>
+        <main>
+          <Switch>
+            <Route path="/about" exact>
+              <About />
+            </Route>
+            <Route path="/contacts" exact>
+              <Contacts />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </main>
+      </div>
+    </Router>
   );
 }
 
